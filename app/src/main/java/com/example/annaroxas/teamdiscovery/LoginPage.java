ecd9774e478ccee7a3bba9a7180519c8069e3b4b
SoSocratic <dale.vincent.chan@gmail.com>
2017-05-22 14:11:42 -0700

    Transition to Login Page
    Transition from main page to Login Page

--- app/src/main/java/com/example/annaroxas/teamdiscovery/LoginPage.java
+++ app/src/main/java/com/example/annaroxas/teamdiscovery/LoginPage.java
@@ -1,10 +1,22 @@
 package com.example.annaroxas.teamdiscovery;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
 /**
  * Created by annaroxas on 2017-05-04.
  */
 
-public class LoginPage {
-	
-	//LOLButts
+public class LoginPage extends AppCompatActivity {
+    private static final String TAG = LoginPage.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if(savedInstanceState == null){
+            Log.d (TAG, "savedInstanceState fucked right up");
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
 }
--- app/src/main/java/com/example/annaroxas/teamdiscovery/System.java
+++ app/src/main/java/com/example/annaroxas/teamdiscovery/System.java
@@ -4,17 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class System extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "Suck a dick dumbshit";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_linear);
     }
 
-    public void startActivity(View view){
+    public void createUser(View view){
         Intent createNewUser = new Intent(this, Create_user.class);
         startActivity(createNewUser);
+    }
+
+    public void startLogin(View view) {
+        /**
+         Intent intent = new Intent(this, DisplayMessageActivity.class);
+         EditText editText = (EditText) findViewById(R.id.editText);
+         String message = editText.getText().toString();
+         intent.putExtra(EXTRA_MESSAGE, message);
+         startActivity(intent);
+         **/
+        Intent logInUser = new Intent(this, LoginPage.class);
+        startActivity(logInUser);
     }
 }
