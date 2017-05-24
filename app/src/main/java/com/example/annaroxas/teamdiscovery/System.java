5ce29d8db48149c3b5adef6827af116c0612efee
Maria Geraldo <annaroxas0326@gmail.com>
2017-05-24 15:02:08 -0700

    used activity_susyem_linear

--- app/src/main/java/com/example/annaroxas/teamdiscovery/System.java
+++ app/src/main/java/com/example/annaroxas/teamdiscovery/System.java
@@ -8,6 +8,6 @@ public class System extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_system);
+        setContentView(R.layout.activity_system_linear);
     }
 }
