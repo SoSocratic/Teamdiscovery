a6bcc26d0903ff0f1405c5899144eb14c693a16d
Horro <kenithjones@hotmail.ca>
2017-06-19 12:38:24 -0700

    PreMerge commit
    ^

---
+++ app/src/main/java/com/example/annaroxas/teamdiscovery/Game_1.java
@@ -0,0 +1,120 @@
+package com.example.annaroxas.teamdiscovery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.ClipData;
+import android.graphics.Typeface;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Game_1 extends AppCompatActivity {
+
+    private ImageView option1, option2, option3, choice1, choice2, choice3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_game);
+
+        //views to drag
+        option1 = (ImageView)findViewById(R.id.sg_a);
+        option2 = (ImageView)findViewById(R.id.sg_c);
+        option3 = (ImageView)findViewById(R.id.sg_t);
+
+        //views to drop onto
+        choice1 = (ImageView)findViewById(R.id.sg_bw_a);
+        choice2 = (ImageView)findViewById(R.id.sg_bw_c);
+        choice3 = (ImageView)findViewById(R.id.sg_bw_t);
+
+        //set touch listeners
+        option1.setOnTouchListener(new ChoiceTouchListener());
+        option2.setOnTouchListener(new ChoiceTouchListener());
+        option3.setOnTouchListener(new ChoiceTouchListener());
+        //set drag listeners
+        choice1.setOnDragListener(new ChoiceDragListener());
+        choice2.setOnDragListener(new ChoiceDragListener());
+        choice3.setOnDragListener(new ChoiceDragListener());
+
+
+    }
+
+
+    private final class ChoiceTouchListener implements OnTouchListener {
+
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                //setup drag
+                ClipData data = ClipData.newPlainText("", "");
+                DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+
+                //start dragging the item touched
+                view.startDrag(data, shadowBuilder, view, 0);
+
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+    }
+
+
+    private class ChoiceDragListener implements OnDragListener {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            //handle drag events
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    //no action necessary
+                    break;
+                case DragEvent.ACTION_DROP:
+                    //handle the dragged view being dropped over a drop view
+                    View view = (View) event.getLocalState();
+
+                    //stop displaying the view where it was before it was dragged
+                    view.setVisibility(View.INVISIBLE);
+                    //view dragged item is being dropped on
+                    TextView dropTarget = (TextView) v;
+                    //view being dragged and dropped
+                    TextView dropped = (TextView) view;
+                    //update the text in the target view to reflect the data being dropped
+                    dropTarget.setText(dropped.getText());
+                    //make it bold to highlight the fact that an item has been dropped
+                    dropTarget.setTypeface(Typeface.DEFAULT_BOLD);
+                    //if an item has already been dropped here, there will be a tag
+                    Object tag = dropTarget.getTag();
+                    //if there is already an item here, set it back visible in its original place
+                    if(tag!=null)
+                    {
+                        //the tag is the view id already dropped here
+                        int existingID = (Integer)tag;
+                        //set the original view visible again
+                        findViewById(existingID).setVisibility(View.VISIBLE);
+                    }
+                    //set the tag in the target view to the ID of the view being dropped
+                    dropTarget.setTag(dropped.getId());
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    //no action necessary
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
+}
--- app/src/main/java/com/example/annaroxas/teamdiscovery/System.java
+++ app/src/main/java/com/example/annaroxas/teamdiscovery/System.java
@@ -14,7 +14,7 @@ public class System extends AppCompatActivity {
     }
 
     public void createUser(View view){
-        Intent createNewUser = new Intent(this, Create_user.class);
+        Intent createNewUser = new Intent(this, Game_1.class);
         startActivity(createNewUser);
     }
 
